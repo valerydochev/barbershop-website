0fec5a506bfb269cc776c658baaedbc6486a9687
valerydochev <valerisdochev@gmail.com>
2026-02-19 17:40:22 +0000

    Setup Tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Tailwind works 🔥</h1>
+    </div>
   )
 }
 
