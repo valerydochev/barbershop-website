8d99831c522d9608df0e0c2aac56b25d068ea25e
valerydochev <valerisdochev@gmail.com>
2026-02-19 17:53:25 +0000

    ,,

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 
 function App() {
+
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Tailwind works 🔥</h1>
-    </div>
+    <>
+      
+    </>
   )
 }
 
